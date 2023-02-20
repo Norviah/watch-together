79143c109dde87d5ae0e72d9b19ec903650e0d43
Norviah <jmasresha@gmail.com>
2023-02-20 15:01:55 -0800

    feat(backend/controllers): implement the user/signup endpoint

--- backend/src/controllers/user.ts
+++ backend/src/controllers/user.ts
@@ -51,6 +51,35 @@ export const router: Router = Router();
  *         description: An internal error has occurred, the specified user was
  *                      not created.
  */
-router.post('/signup', async (request: Request, response: Response) => {
-  return response.status(StatusCodes.OK).json({ message: 'Hello World' });
+router.post('/signup', async (request: Request<unknown, unknown, { email: string; password: string; firstName: string; lastName: string }>, response: Response) => {
+  // The `user/signup` endpoint, this endpoint implements the logic for creating
+  // a new user within the database, referencing information from the request
+  // body.
+
+  // First, we'll need to ensure that the user does not already exist within the
+  // database. The `User` model has a unique contstraint on the `email` field,
+  // meaning that we can use this property to find a user within the database.
+  const exists: User | null = await client.user.findUnique({
+    where: {
+      email: request.body.email,
+    },
+  });
+
+  // Prisma will return `null` if an entry does not exist within the database,
+  // meaning if `null` was not returned, the user already exists within the
+  // database. We'll return a `409` status code to indicate that the user
+  // already exists.
+  if (exists) {
+    return response.status(StatusCodes.CONFLICT).json({ message: 'The specified user already exists.' });
+  }
+
+  // If the user does not exist within the database, we can create a new user
+  // within the database with the provided information.
+  await client.user.create({ data: { ...request.body } });
+
+  // Once the user has been created, we'll return a `200` status code to
+  // indicate that the user was created successfully. We don't need to catch any
+  // exceptions here, as we have a global error handler that will catch any
+  // errors that occur within the application.
+  return response.status(StatusCodes.OK).json({ message: 'The specified user was created.' });
 });
