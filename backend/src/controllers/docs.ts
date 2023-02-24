87ba5ad53c5ca761de8fbd6790239961569d100f
norviah <joseph>
2023-02-23 19:11:04 -0800

    docs(backend/api): remove documentation for 'docs/swagger.json'

--- backend/src/controllers/docs.ts
+++ backend/src/controllers/docs.ts
@@ -6,18 +6,6 @@ import type { Request, Response } from 'express';
 
 export const router: Router = Router();
 
-/**
- * @openapi
- * /docs/swagger.json:
- *   get:
- *     summary: The OpenAPI specification.
- *     description: Get the OpenAPI specification.
- *     responses:
- *       200:
- *         description: The OpenAPI specification.
- *       500:
- *         description: An internal error has occurred.
- */
 router.get('/swagger.json', (request: Request, response: Response) => {
   response.json(require(OPENAPI));
 });
