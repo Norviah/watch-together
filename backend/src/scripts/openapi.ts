94b11a21b1567f0abb078310515fcd0f8fdbc3cd
norviah <joseph>
2023-02-23 17:15:01 -0800

    feat(backend/docs): add more metadata information about the api documentation

--- backend/src/scripts/openapi.ts
+++ backend/src/scripts/openapi.ts
@@ -3,17 +3,32 @@ import { writeFileSync } from 'fs';
 import { logger } from '@/structs/Logger';
 import swaggerJsdoc from 'swagger-jsdoc';
 
+import type { Options } from 'swagger-jsdoc';
+
 import * as paths from '@/util/paths';
 
-const options = {
+const options: Options = {
   failOnErrors: true,
+
   definition: {
     openapi: '3.0.0',
     info: {
       title: 'Watch Together',
       version: '0.1.0',
+      license: {
+        name: 'MIT',
+        url: 'https://raw.githubusercontent.com/Norviah/watch-together/master/LICENSE',
+      },
     },
+    'x-tagGroups': [
+      {
+        name: 'User Management',
+        tags: ['user'],
+      },
+    ],
+    tags: [{ name: 'user', description: 'Operations related to a user.' }],
   },
+
   apis: [join(paths.CONTROLLERS, '**/*.ts')],
 };
 
